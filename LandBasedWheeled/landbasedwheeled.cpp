3385bbdeb9e1e893ae0ea6dfa5f3fbec5d0ccaa0
Prateek Bhargava <60759915+pbhargav96@users.noreply.github.com>
2020-11-08 23:50:10 -0500

    Add files via upload

---
+++ LandBasedWheeled/landbasedwheeled.cpp
@@ -0,0 +1,5 @@
+//
+// Created by amoghavarsha on 11/8/20.
+//
+
+#include "landbasedwheeled.h"
---
+++ LandBasedWheeled/landbasedwheeled.h
@@ -0,0 +1,14 @@
+//
+// Created by amoghavarsha on 11/8/20.
+//
+
+#ifndef MAZE_ROBOT_LANDBASEDWHEELED_H
+#define MAZE_ROBOT_LANDBASEDWHEELED_H
+
+
+class landbasedwheeled {
+
+};
+
+
+#endif //MAZE_ROBOT_LANDBASEDWHEELED_H
