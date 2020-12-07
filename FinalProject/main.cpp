959e6f238e83bd61eecb7189f964cd0c16968172
AmoghavarshaP <aprasann@terpmail.umd.edu>
2020-12-07 02:13:15 -0500

    made small changes to main file

--- FinalProject/main.cpp
+++ FinalProject/main.cpp
@@ -1,9 +1,25 @@
+/**
+* @file main.cpp
+* @authors
+* Group 4
+* Prateek Bhargava, UID : 116947992
+* Amoghavarsha Prasanna, UID : 116952910
+*
+* @version 1.0
+*
+* @section File Description
+* main file
+*
+*/
+
+
 #include <iostream>
 #include <memory>
 #include "src/Algorithm/algorithm.h"
 #include "src/API/api.h"
 #include "src/Maze/maze.h"
 #include "src/LandBasedWheeled/landbasedwheeled.h"
+#include "src/LandBasedTracked/landbasedtracked.h"
 #include <chrono>
 
 int main() {
